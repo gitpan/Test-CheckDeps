b5161aa0d4a87f18fede7737587e3fe9921fc2b1
Leon Timmermans <fawaka@gmail.com>
2013-09-25 11:51:48 +0000

    Import of LEONT/Test-CheckDeps-0.008 from CPAN.
    gitpan-cpan-distribution: Test-CheckDeps
    gitpan-cpan-version:      0.008
    gitpan-cpan-path:         LEONT/Test-CheckDeps-0.008.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

--- lib/Test/CheckDeps.pm
+++ lib/Test/CheckDeps.pm
@@ -1,6 +1,6 @@
 package Test::CheckDeps;
 {
-  $Test::CheckDeps::VERSION = '0.007';
+  $Test::CheckDeps::VERSION = '0.008';
 }
 use strict;
 use warnings FATAL => 'all';
@@ -71,7 +71,7 @@ Test::CheckDeps - Check for presence of dependencies
 
 =head1 VERSION
 
-version 0.007
+version 0.008
 
 =head1 DESCRIPTION
 
